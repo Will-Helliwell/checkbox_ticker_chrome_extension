89385039ef23f399a5a0986d8c5c786598c03221
Will-Helliwell <wj.helliwell@gmail.com>
2025-01-22 12:50:47 +0800

    Now ticks all boxes in the current tab.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-document.getElementById('tickAllCheckboxes').addEventListener('click', tickAllCheckboxes);
+document.getElementById('tickAllCheckboxes').addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    function: tickAllCheckboxes
+  });
+});
 
 function tickAllCheckboxes() {
-    console.log("in tickAllCheckboxes");
-    
-  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].checked = true;
-  }
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  let count = 0;
+  
+  checkboxes.forEach(checkbox => {
+    checkbox.checked = true;
+    count++;
+  });
+  
+  console.log(`Ticked ${count} checkboxes`);
 }
